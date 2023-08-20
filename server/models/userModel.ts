71958f89bb597f91cc62de1c10d120df804251ef
Rishu jha <jharishu796@gmail.com>
2023-08-20 18:40:10 +0000

    backend:- login, logout and get refresh_token endpoints/functionality implemented

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,36 +1,41 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
+import { IUser } from "../config/interface";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true,"Please add your name."],
-        trim: true,
-        maxLength: [20,"Your name is upto 20 chars long."]
+      type: String,
+      required: [true, "Please add your name."],
+      trim: true,
+      maxLength: [20, "Your name is upto 20 chars long."],
     },
     account: {
-        type: String,
-        required: [true,"Please add your email or phone."],
-        trim: true,
-        unique: true
+      type: String,
+      required: [true, "Please add your email or phone."],
+      trim: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: [true,"Please add your password."],
+      type: String,
+      required: [true, "Please add your password."],
     },
     avatar: {
-        type: String,
-        default: 'https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png'
+      type: String,
+      default:
+        "https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png",
     },
     role: {
-        type: String,
-        default: "user" // admin
+      type: String,
+      default: "user", // admin
     },
     type: {
-        type: String,
-        default: "normal" //fast
-    }
-},{
-    timestamps: true
-})
+      type: String,
+      default: "register", //login
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export default mongoose.model('User',userSchema);
+export default mongoose.model<IUser>("User", userSchema);
