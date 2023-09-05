d8347f0dac4febf47098646bb52dd3e847d379fc
Rishu jha <jharishu796@gmail.com>
2023-09-05 18:51:42 +0000

    conflict resolved

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,6 +5,7 @@ import Header from "./components/global/Header";
 import Footer from "./components/global/Footer";
 import Alert from "./components/alert/Alert";
 
+
 function App() {
   return (
     <div>
