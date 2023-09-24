f99658c6270e942aee84f2bf16f0d09c2eac0e7b
Rishu jha <jharishu796@gmail.com>
2023-09-24 05:14:48 +0000

    google login completed frontend

--- my-app/src/components/auth/SocialLogin.tsx
+++ my-app/src/components/auth/SocialLogin.tsx
@@ -1,12 +1,19 @@
-import { GoogleLogin, GoogleLoginResponse } from "react-google-login-lite";
+import { useDispatch } from "react-redux";
+// import { GoogleLogin, GoogleLoginResponse } from "react-google-login-lite";
+import { GoogleLogin, GoogleLoginResponse } from "./google";
+
+import { googleLogin } from "../../redux/actions/authAction";
 
 function SocialLogin() {
+  const dispatch = useDispatch();
   const onSuccess = (googleUser: GoogleLoginResponse) => {
-    console.log(googleUser);
+    // console.log(googleUser);
+    const id_token = googleUser.getAuthResponse().id_token;
+    dispatch(googleLogin(id_token));
   };
 
   return (
-    <div>
+    <div style={{ width: "100%" }}>
       <GoogleLogin
         client_id={`${process.env.REACT_APP_MAIL_CLIENT_ID}`}
         cookiepolicy="single_host_origin"
