8bfa8f6fd7e2cdd9cb92a5b155cb142f4a931b36
123rishujha <jharishu796@gmail.com>
2023-10-01 17:21:31 +0530

    profile page ui frontend

---
+++ my-app/src/components/profile/OtherInfo.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const OtherInfo = () => {
+  return (
+    <div>OtherInfo</div>
+  )
+}
+
+export default OtherInfo
+// modified
---
+++ my-app/src/components/profile/UserBlogs.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const UserBlogs = () => {
+  return (
+    <div>UserBlogs</div>
+  )
+}
+
+export default UserBlogs
+//modified
