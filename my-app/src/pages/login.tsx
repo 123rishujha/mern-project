fb19f01b87bf89f4b8ea25874251e084866494a2
123rishujha <jharishu796@gmail.com>
2023-10-01 12:58:49 +0530

    rishu quictouch removed

--- my-app/src/pages/login.tsx
+++ my-app/src/pages/login.tsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 
 import LoginPass from "../components/auth/LoginPass";
 import LoginSMS from "../components/auth/LoginSMS";
-import SocialLogin from "../components/auth/SocialLogin";
+// import SocialLogin from "../components/auth/SocialLogin";
 
 import { RootStore } from "../utils/TypeScript";
 
@@ -21,7 +21,7 @@ const Login = () => {
     <div className="auth_page">
       <div className="auth_box">
         <h3 className="text-uppercase text-center mb-4">Login</h3>
-        <SocialLogin />
+        {/* <SocialLogin /> */}
         {sms ? <LoginSMS /> : <LoginPass />}
         <small className="row my-2 text-primary" style={{ cursor: "pointer" }}>
           <span className="col-6">
