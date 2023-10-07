ce6cfd4525fb09f917275ae5ec812b173f4f888e
Rishu jha <jharishu796@gmail.com>
2023-10-07 19:09:09 +0000

    backend :- category crud completed

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -5,13 +5,16 @@ import { IDecodedToken, IReqAuth } from "../config/interface";
 
 const auth = async (req: IReqAuth, res: Response, next: NextFunction) => {
   const token = req.header("Authorization");
+  console.log("token", token);
   if (!token) return res.status(400).json({ msg: "Invalid Authentication" });
   try {
     const decoded = <IDecodedToken>(
       jwt.verify(token, `${process.env.ACCESS_TOKEN_SECRET}`)
     );
     if (!decoded)
-      return res.status(400).json({ msg: "Invalid Authentication" });
+      return res
+        .status(400)
+        .json({ msg: "You Are Not Authorised For This Operation" });
 
     const user = await Users.findOne({ _id: decoded.id });
     if (!user) return res.status(400).json({ msg: "User does not exist" });
