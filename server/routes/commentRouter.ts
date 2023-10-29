f77ad17c5252112392a04506702d6228b6a6d54c
Rishu jha <jharishu796@gmail.com>
2023-10-29 18:03:10 +0000

    replying comment

--- server/routes/commentRouter.ts
+++ server/routes/commentRouter.ts
@@ -5,6 +5,7 @@ import auth from "../middleware/auth";
 const router = express.Router();
 
 router.post("/comment", auth, commentCtrl.createComment);
-router.get('/comments/blog/:id', commentCtrl.getComments)
+router.get("/comments/blog/:id", commentCtrl.getComments);
+router.post("/reply_comment", auth, commentCtrl.replyComment);
 
 export default router;
